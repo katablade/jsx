9c601b6d4d14979899fadc3e8cef42ec89fb630b
katablade <57857686+katablade@users.noreply.github.com>
2020-03-08 11:13:07 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import imageInSrc from "./katablade.jpg";
+import './App.css';
+
+function App() {
+  return (
+    <div className="app" style={{border:"solid 1px black", maxWidth: "100vw"}}>
+	  <h1 className="title red">My title</h1>
+    <br />
+    <img src={imageInSrc} alt="img" />
+    <br />
+	  <img src={imageInSrc} alt="img" />
+    </div>
+
+  );
+}
+
+export default App;
