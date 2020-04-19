93604587418e292ce52d4cff550676c7bc782a34
katablade <57857686+katablade@users.noreply.github.com>
2020-04-19 11:04:58 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -4,14 +4,18 @@ import './App.css';
 
 function App() {
   return (
+    <div className="app">
     <div className="app" style={{border:"solid 1px black", maxWidth: "100vw"}}>
-	  <h1 className="title red">My title</h1>
+	  <h1 className="title red">Hazem</h1>
     <br />
     <img src={imageInSrc} alt="img" />
     <br />
 	  <img src="/katablade.jpg" alt="img" />
     </div>
-
+    <video width="320" height="240" controls >
+    <source src="impossible.mp4" type="video/mp4" />
+    </video>
+    </div>
   );
 }
 
